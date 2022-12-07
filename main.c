6e47e6d53df8cdec9164e96eac0ce1ab30c75ea7
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-12-07 17:24:18 +0900

    실습3 동적메모리접근및반납

--- main.c
+++ main.c
@@ -14,3 +14,19 @@ void main(void) {
 	for (i=0;i<3;i++)
 		bookshelf[i] = (struct Book*)malloc(10 * sizeof (struct Book));
 }
+
+bookshelf[1][3].number = 5;
+strcpy(bookshelf[1][3].title, "C++ Programming");
+
+(bookshelf[2]+4)->number = 3;
+strcpy((bookshelf[2]+4)->title, "Communications Theory");
+
+printf("book (1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+printf("book (2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
+
+for (i=0;i<3;i++)
+	free(bookshelf[i]);
+	free(bookshelf);
+return; 
+}
+
